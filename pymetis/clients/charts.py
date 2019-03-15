5ac727cb30f387c701a3da18f5a1e2f0935215f2
root <root@bi-analysis-jp01.novalocal>
2019-03-15 09:43:32 +0000

    addition metatron auth

--- pymetis/clients/charts.py
+++ pymetis/clients/charts.py
@@ -2,6 +2,21 @@ import requests
 
 
 def get(hostname, port, did):
-    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+    loginHeaders = {'Authorization': 'Basic cG9sYXJpc19jbGllbnQ6cG9sYXJpcw==', 'Content-Type' : 'application/x-www-form-urlencoded'}
+    loginData = {'grant_type':'password', 'scope':'write', 'username':'polaris', 'password':'polaris'}
+    loginUrl = "http://" + hostname + ":" + port + "/oauth/token"
+    res = requests.post(loginUrl, loginData,  headers=loginHeaders).json()
+
+    # login check
+    #print(res)
+
+    token_type = res["token_type"]
+    token = res["access_token"]
+    auth = token_type + ' ' + token
+
+    #auth value check
+    #print('auth:' + auth)
+
+    headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': auth}
     url = "http://" + hostname + ":" + port + "/api/widgets/" + did + "/data"
     return requests.post(url, headers=headers).json()
